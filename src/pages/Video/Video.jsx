410fb548125759022ce588bfa55498a055015fe0
DamianoL <damianolunardo@hotmail.it>
2024-02-26 08:44:42 +0100

    Add - scroll top for selected video

--- src/pages/Video/Video.jsx
+++ src/pages/Video/Video.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
-import './Video.css'
-import PlayVideo from '../../Components/PlayVideo/PlayVideo'
-import Recommended from '../../Components/Recommended/Recommended'
-import { useParams } from 'react-router-dom'
+import React, { useRef, useEffect } from 'react';
+import './Video.css';
+import PlayVideo from '../../Components/PlayVideo/PlayVideo';
+import Recommended from '../../Components/Recommended/Recommended';
+import { useParams } from 'react-router-dom';
 
 const Video = () => {
+  const { videoId, categoryId } = useParams();
+  const videoRef = useRef(null);
 
-  const {videoId,categoryId} = useParams();
+  useEffect(() => {
+    // Scrolla quando il video viene caricato
+    if (videoRef.current) {
+      videoRef.current.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  }, [videoRef, videoId]);
 
   return (
-    <div className='play-container'>
+    <div className='play-container' ref={videoRef}>
       <PlayVideo videoId={videoId} />
       <Recommended categoryId={categoryId} videoId={videoId}/>
-
     </div>
-  )
-}
+  );
+};
 
-export default Video
+export default Video;
